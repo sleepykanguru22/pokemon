ee6bbcb7fbaa22f49b049285b06cc1968d4e1d9b
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-07-14 14:35:20 -0700

    initial commit

---
+++ src/components/Pokemon.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+const Pokemon = (props) => {
+  const [pokemon, setPokemon] = useState(null);
+
+  const getPokemon = (event) => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setPokemon(response.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const displayPokemon = () => {
+    if (pokemon === null) {
+      return;
+    } else {
+      return pokemon.map((pokemon, idx) => {
+        return (
+          <li key={idx}>
+            {idx + 1}: {pokemon.name}
+          </li>
+        );
+      });
+    }
+  };
+
+  return (
+    <>
+      <button onClick={getPokemon}>Fetch Pokémon!</button>
+      <ul>{displayPokemon()}</ul>
+    </>
+  );
+};
+
+export default Pokemon;
